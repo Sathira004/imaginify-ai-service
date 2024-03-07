a1224d93a29402ed437241c6ba07ba220319f711
Sathira004 <sathirabandara1@gmail.com>
2024-03-07 23:32:23 +0530

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,12 @@
-import Image from "next/image";
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
 
-export default function Home() {
+const Home = () => {
   return (
-    <div>Home</div>
-  );
+    <div>
+      <UserButton afterSignOutUrl='/' />
+    </div>
+  )
 }
+
+export default Home
