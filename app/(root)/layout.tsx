42111376bc8a24515d227dcd0798cc3fde502f13
Sathira004 <sathirabandara1@gmail.com>
2024-03-10 10:23:57 +0530

    update

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,12 @@
-import SideBar from "@/components/shared/SideBar";
+import MobileNav from "@/components/shared/MobileNav";
+import SideBar from "@/components/shared/Sidebar";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
       <SideBar />
+      <MobileNav />
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
